80b3e6b9b40afdcf365879021ea90f1c5150bd55
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2024-06-24 02:27:42 +0300

    Add app screens

---
+++ screens/NewTweetScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function NewTweetScreen() {
+    return (
+        <View>
+            <Text>test12345</Text>
+        </View>
+    );
+}
